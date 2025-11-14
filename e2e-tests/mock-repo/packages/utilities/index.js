84f0c695fc60c5b5665bb700add1b330a36fc31d
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2025-11-14 11:43:18 -0500

    Initial mock repo

---
+++ e2e-tests/mock-repo/packages/utilities/index.js
@@ -0,0 +1,13 @@
+import depA from 'dep-a';
+import depB from 'dep-b';
+import depC from 'dep-c';
+
+export default {
+  name: 'utilities',
+  helper: () => {
+    console.log('Running utilities');
+    console.log(depA.greet());
+    console.log(depB.greet());
+    console.log(depC.greet());
+  }
+};
